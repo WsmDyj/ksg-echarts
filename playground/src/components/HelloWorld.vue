c64c0fc11380a0ffabaa8180b94feca08a6ed982
wusimin <wusimin@kuaishou.com>
2024-06-29 00:12:56 +0800

    feat: 文档生成

---
+++ playground/src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+ 
+  <div>
+     
+    <PieChart :data="chartData" :option="option"></PieChart>
+  </div>
+</template>
+<script setup>
+import { ref} from 'vue'
+import { PieChart } from '../../../packages/index'
+
+const option = ref({
+      title: {
+        text: "Traffic Sources",
+        left: "center"
+      },
+      tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
+      
+    })
+
+    const chartData = ref({
+          dimensions: {
+            name: "渠道",
+            data: ["Direct", "Email", "Ad Networks", "Video Ads", "Search Engines"]
+          },
+          measures: [
+            {
+              name: "Traffic Sources",
+              data: [10000, 27800, 22000, 20200, 15600]
+            }
+          ]
+        })
+</script>
