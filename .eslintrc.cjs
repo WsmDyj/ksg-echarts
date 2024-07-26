61164ffb7d605bb5fe1010d2b5702a0b2e3c1f3a
wusimin <wusimin@kuaishou.com>
2024-07-26 21:46:07 +0800

    fix: 去除敏感信息

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,3 @@
-/*
- * @Author: wusimin 
- * @Date: 2024-06-26 15:05:56
- * @LastEditors: wusimin wusimin@kuaishou.com
- * @LastEditTime: 2024-07-03 10:00:59
- * @FilePath: /kwaida/packages/kwaida-charts/.eslintrc.cjs
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 /* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution');
 
--- packages/base/props.ts
+++ packages/base/props.ts
@@ -1,12 +1,3 @@
-/*
- * @Author: wusimin wusimin@kuaishou.com
- * @Date: 2024-07-03 10:48:03
- * @LastEditors: wusimin wusimin@kuaishou.com
- * @LastEditTime: 2024-07-03 19:42:37
- * @FilePath: /ksg-echarts/packages/base/props.ts
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-
 import { PropType } from 'vue';
 import { InitOptions, LoadingOptions, Option, Theme, UpdateOptions } from '../types';
 
