dca8bfccb204e307bf2a005bc47f73617e7b4df2
wusimin <wusimin@kuaishou.com>
2024-07-26 18:37:12 +0800

    docs: 文档修复

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -4,7 +4,7 @@ import { useComponents } from './useComponents'
 import './style/var.css';
 import './style/custom.css';
 import ksgEchart from 'ksg-echarts';
-import 'ksg-echarts/dist/es/style.css'
+import '../../node_modules/ksg-echarts/dist/es/style.css'
 export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
